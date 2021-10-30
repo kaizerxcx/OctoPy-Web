2de368eb90eb5ae792aec8f34503875c53d7bfb3
Kaizer <johnkaizer.tabanag@cit.edu>
2021-10-31 01:16:04 +0800

    api update

--- OctoPy/api/urls.py
+++ OctoPy/api/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('getUsers/', views.getAllUser, name="user-list"),
+	path('getAllUser/', views.getAllUser, name="users-list"),
+	path('getUser/<str:pk>/', views.getUser, name="user-info"),
 	path('verifyUser/<str:username>/<str:password>/', views.verifyUser, name="verify-user"),
 	path('registerUser/<str:firstname>/<str:lastname>/<str:age>/<str:username>/<str:password>/', views.registerUser, name="register-user"),
+	path('updateUser/<str:user_id>/<str:firstname>/<str:lastname>/<str:age>/<str:username>/<str:password>/', views.updateUser, name="update-user"),
+	path('deleteUser/<str:user_id>/', views.deleteUser, name="delete-user"),
 ]
 
--- OctoPy/api/views.py
+++ OctoPy/api/views.py
@@ -1,15 +1,24 @@
+from django.core.serializers import serialize
 from django.shortcuts import render
 from user.models import *
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
 import hashlib
+from django.core import serializers
+from django.http import HttpResponse
 
 # Create your views here.
 
 @api_view(['GET'])
 def getAllUser(request):
 	user = User.objects.all()
+	serializer = UserSerializer(user, many=True)
+	return Response(serializer.data)
+
+@api_view(['POST'])
+def getUser(request, pk):
+	user = User.objects.filter(user_id=pk)
 	serializer = UserSerializer(user, many=True)
 	return Response(serializer.data)
 
@@ -34,3 +43,21 @@ def registerUser(request, firstname, lastname, age, username, password):
     user = Child.objects.create(firstname = firstname,lastname = lastname, age = age, username = username, password = password)
     Points.objects.create(child_id_id = user.child_id)
     return Response("ok")
+
+@api_view(['POST'])
+def updateUser(request, user_id, firstname, lastname, age, username, password):
+    str = password
+    md5pass = hashlib.md5(password.encode())
+    password = md5pass.hexdigest()
+    if str:
+        update = User.objects.filter(user_id = user_id).update(firstname = firstname, lastname = lastname, age = age, username = username, password = password)
+    else:
+        update = User.objects.filter(user_id = user_id).update(firstname = firstname, lastname = lastname, age = age, username = username)
+    user = User.objects.filter(user_id= user_id)
+    serializer = UserSerializer(user, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def deleteUser(request, user_id):
+    update = User.objects.filter(user_id = user_id).delete()
+    return Response("deleted")
